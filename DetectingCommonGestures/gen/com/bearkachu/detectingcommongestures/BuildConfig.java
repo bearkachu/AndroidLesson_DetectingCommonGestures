66e98ed4c702d1b304f655e406c9c339bca37ee6
bearkachu <bearkachu@gmail.com>
2013-01-29 18:13:47 +0900

    Initial commit
    [20130129 Inwon]
    The application is detecting motion as follows.
     1. Touch UP
     2. Touch DOWN
     3. Touch MOVE
     4. Touch CANCEL
     5. Touch OUTSIDE

---
+++ DetectingCommonGestures/gen/com/bearkachu/detectingcommongestures/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.bearkachu.detectingcommongestures;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ DetectingCommonGestures/src/com/bearkachu/detectingcommongestures/MainActivity.java
@@ -0,0 +1,68 @@
+package com.bearkachu.detectingcommongestures;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v4.view.MotionEventCompat;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MotionEvent;
+
+public class MainActivity extends Activity {
+	// This example shows an activity, but you would use the same approach if
+	// you were subclassing a View
+	private static final String DEBUG_TAG = null;
+	private CustomedTouchPad cTPad = new CustomedTouchPad();
+	private long downTime;
+	private long eventTime;
+	private float x;
+	private float pressure;
+	private float y;
+	private float size;
+	private int metaState;
+	private float xPrecision;
+	private float yPrecision;
+	private int deviceId;
+	private int edgeFlags;
+	
+	@Override
+    public boolean onTouchEvent(MotionEvent event) {
+		int action = MotionEventCompat.getActionMasked(event);
+		
+		switch(action) {
+		case (MotionEvent.ACTION_DOWN):
+			Log.d(DEBUG_TAG, "Action was DOWN");
+		    return true;
+		case (MotionEvent.ACTION_MOVE):
+            cTPad.setX(event.getX());
+            cTPad.setY(event.getY());
+            // send USB-CDC data from here!! 
+            System.out.println("x=" + cTPad.getX() + ", y=" + cTPad.getY() );
+			Log.d(DEBUG_TAG, "Action was MOVE");
+			return true;
+		case (MotionEvent.ACTION_UP):
+			Log.d(DEBUG_TAG, "Action was UP");
+			return true;
+		case (MotionEvent.ACTION_CANCEL):
+			Log.d(DEBUG_TAG, "Action was CANCEL");
+			return true;
+		case (MotionEvent.ACTION_OUTSIDE):
+			Log.d(DEBUG_TAG, "Movement occurred outside of bounds " + "of currnet screen element");
+			return true;
+		default:
+			return super.onTouchEvent(event);	
+		} 	
+    }
+	
+	@Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.activity_main, menu);
+        return true;
+    }    
+}
